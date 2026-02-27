8976ce7279ac9eed6c1811e30a904a1c8f57177a
adeebatgithub <adeebdanish063@gmail.com>
2026-02-27 10:03:59 +0530

    patch 1.7.5 - ui update
    - blocker
    - teachers can see only their profile

--- academics/middlewares.py
+++ academics/middlewares.py
@@ -1,12 +1,17 @@
 from django.contrib import messages
 
+from academics.models import AcademicYear
+
 
 class CheckForAcademicYearMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.session.get("is_academic_year_set") == 0:
+        if AcademicYear.objects.filter(is_active=True).count() == 0:
+            request.session["is_academic_year_set"] = 0
             messages.info(request, "Set Academic Year")
             return self.get_response(request)
+
+        request.session["is_academic_year_set"] = 1
         return self.get_response(request)
