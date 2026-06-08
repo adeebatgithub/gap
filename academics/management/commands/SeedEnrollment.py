cf9d4c1c24d20e001a885368423e8fb55be59390
adeebatgithub <adeebdanish063@gmail.com>
2026-06-08 15:58:11 +0530

    rev 1.0.6

--- academics/management/commands/SeedEnrollment.py
+++ academics/management/commands/SeedEnrollment.py
@@ -1,29 +1,61 @@
-import random
-
 from django.core.management.base import BaseCommand
+from openpyxl import load_workbook
 
-from academics.academicyear.models import AcademicYear
-from academics.enrollment.models import Student, Enrollment
+from django.contrib.auth import get_user_model
 from academics.schoolclass.models import SchoolClass
+from academics.enrollment.models import Student, Enrollment
+from controller.utils import get_academic_year
 
 
 class Command(BaseCommand):
-    help = "Enroll students"
+    help = "Import teachers from an Excel file"
 
-    def handle(self, *args, **kwargs):
-        students = Student.objects.all()
-        classes = SchoolClass.objects.all()
-        enrollments = []
-        for student in students:
-            enrollment = Enrollment(
-                student=student,
-                school_class=random.choice(classes),
-                academic_year=AcademicYear.objects.get(is_active=True)
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "file_path",
+            type=str,
+            help="Path to the Excel file"
+        )
+
+    def handle(self, *args, **options):
+        file_path = options["file_path"]
+
+        wb = load_workbook(file_path)
+        ws = wb.active
+
+        # Read header row
+        headers = [cell.value for cell in ws[3]]
+        print(headers)
+
+        try:
+            name_idx = headers.index("Name")
+            class_idx = headers.index("Class")
+        except ValueError:
+            self.stdout.write(
+                self.style.ERROR("Name or Email column not found")
             )
-            enrollments.append(enrollment)
+            return
 
-        Enrollment.objects.bulk_create(enrollments)
+        for row in ws.iter_rows(min_row=4, values_only=True):
+            name = row[name_idx]
+            class_name = row[class_idx]
+            if not class_name or not name:
+                continue
+            if "ALUMNI" in class_name:
+                continue
+            self.stdout.write(self.style.SUCCESS(f"Name: {name}, class_name: {class_name}"))
+
+            _class, _ = SchoolClass.objects.get_or_create(name=class_name.strip().lower(), academic_year_id=get_academic_year())
+            student = Student.objects.create(
+                name=name.strip().title(),
+            )
+            Enrollment.objects.create(
+                student=student,
+                school_class=_class,
+            )
+
+
 
         self.stdout.write(
-            self.style.SUCCESS("Successfully Enrolled students")
+            self.style.SUCCESS("Import completed successfully")
         )
